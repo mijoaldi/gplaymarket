233bd7f76a1cc2eec97fe9068aa329819342e9bc
Esneyder Quiñones <scneiderck@gmail.com>
2021-09-23 21:28:55 -0500

    restablecer

---
+++ Recursos/php/conexion_be.php
@@ -0,0 +1,12 @@
+<?php 
+
+$conexion=mysqli_connect("localhost", "root", "Alejo0501", "gplaymarket");
+
+
+// if($conexion){
+//     echo 'conectado exitosamente a la base de datos';
+// }else{
+//     echo 'no se ha conectado correctamente';
+// }
+
+?>
